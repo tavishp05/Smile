52123d213aef1869da4416080e7b69e9e6d20165
Tavish <18051@wc.school.nz>
2022-03-08 15:10:41 +1300

    Update app with new web page routes

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
-app = Flask(__name__)
+
+ app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+ @app.route('/')
+ def render_homepage():
+     return render_template("home.html")
 
+ @app.route('/menu')
+ def render_homepage():
+     return render_template("menu.html")
 
-app.run(host = '0.0.0.0')
+ @app.route('/contact')
+ def render_homepage():
+     return render_template("contact.html")
+
+ app.run(host="0.0.0.0")
