17d02d4331e6498646798376a567f2c03a36f382
Tavish <18051@wc.school.nz>
2022-03-08 15:22:05 +1300

    Update app with new web page routes (proper formatting)

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
- app = Flask(__name__)
+app = Flask(__name__)
 
 
- @app.route('/')
- def render_homepage():
-     return render_template("home.html")
+@app.route('/')
+def render_homepage():
+    return render_template("home.html")
 
- @app.route('/menu')
- def render_homepage():
-     return render_template("menu.html")
 
- @app.route('/contact')
- def render_homepage():
-     return render_template("contact.html")
+@app.route('/menu')
+def render_menu_page():
+    return render_template("menu.html")
 
- app.run(host="0.0.0.0")
+
+@app.route('/contact')
+def render_contact_page():
+    return render_template("contact.html")
+
+
+app.run(host="0.0.0.0")
